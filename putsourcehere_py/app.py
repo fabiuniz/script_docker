47687db91e2379683f6e9bdcb68a40c7edab6721
Fabiano <git@git.com.br>
2025-02-10 16:44:40 -0300

    Instruções para clonar

---
+++ putsourcehere_py/app.py
@@ -0,0 +1,19 @@
+    import ssl
+    from flask import Flask
+    from flask_cors import CORS
+    app = Flask(__name__)
+    #>- Configura o CORS para permitir todas as origens e credenciais <br>
+    CORS(app, supports_credentials=True)
+    @app.route('/')
+    def index():
+        return "Hello World!"
+    def runFlaskport(app, debug, host, port):
+        #>- Caminho para o certificado SSL e a chave privada <br>
+        ssl_cert = 'ssl/nginx-ssl.crt'
+        ssl_key = 'ssl/nginx-ssl.key'
+        #>- Configurações de contexto SSL <br>
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+        ssl_context.load_cert_chain(ssl_cert, ssl_key)
+        app.run(ssl_context=ssl_context, debug=debug, host=host, port=port)
+    if __name__ == '__main__':
+        runFlaskport(app, False, '0.0.0.0', 8000)
