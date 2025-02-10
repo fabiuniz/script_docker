9eb3b55aaf8396b0ae4488baa2201aa64365055d
Fabiano <git@git.com.br>
2025-02-10 15:19:21 -0300

    Instruções para clonar

--- setup_script_launcher_py.sh
+++ setup_script_launcher_py.sh
@@ -23,6 +23,7 @@ dos2unix "$appcontainer/script.cfg" #<--------------------------
 source "$appcontainer/script.cfg" #<--------------------------
 #>- construindo .sh para Iniciar docker <br>
 cat <<EOF > start_$app_name.sh
+    source scripts/lib_bash.sh
     # - app_name="${app_name}"
     docker_compose_file="docker-compose.yml"
     #>-  - Construir e subir os containeres <br>
@@ -32,7 +33,7 @@ cat <<EOF > start_$app_name.sh
     echo "docker exec --privileged -it "$app_name"_nginx bash"
     echo "docker logs "$app_name"_nginx"
     echo "docker logs --tail 10 "$app_name"_app"    
-    echo "Aplicação $app_name est rodando em http://localhost e https://localhost"
+    echo "$app_dir Aplicação $app_name está rodando em http://$name_host$app_port e https://$name_host:$app_port" 
     #>-  - Nota: Caso o serviço Apache ou Nginx já existente esteja usando as portas 80 e 443, <br>
     #>-  - certifique-se de parar ou reconfigur-lo para evitar conflitos de porta. <br>
 EOF
